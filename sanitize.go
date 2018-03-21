fdbd663f4045f426478a15f3859723d18e06a543
Artyom Pervukhin <artyom@evasive.ru>
2018-03-21 09:46:37 -0300

    Initial commit

---
+++ sanitize.go
@@ -0,0 +1,176 @@
+// Package sanitize provides functions to sanitize (anonymize) certain string
+// fields of arbitrary json messages.
+//
+// Note that the main use case for this package is handling of opaque json
+// messages, not anything with the known structure, which is better handled
+// explicitly by sanitizing data and then marshaling sanitized representation.
+package sanitize
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"regexp"
+	"strconv"
+)
+
+var errInvalidArguents = errors.New("sanitize: either field set or regexp must be provided")
+
+// Stream sanitizes json payload read from r writing result to w. fset is
+// a set of exact field names to be filtered, re is a regular expression to
+// match against field names to be filtered. Either of fset or re can be nil,
+// but not both. Matched string fields are replaced with Mask value.
+//
+// For smaller messages it is more effective to use Message function.
+func Stream(w io.Writer, r io.Reader, fset map[string]struct{}, re *regexp.Regexp) error {
+	if re == nil && len(fset) == 0 {
+		return errInvalidArguents
+	}
+	bw := bufio.NewWriter(w)
+	defer bw.Flush()
+	dec := json.NewDecoder(r)
+	dec.UseNumber()
+	var ds []rune // stack of separators
+	var cnt int
+	var sanitize bool
+	var prevDelim byte
+	var tmp []byte
+	for {
+		var delim byte = comma
+		t, err := dec.Token()
+		if err == io.EOF {
+			return bw.Flush()
+		}
+		if err != nil {
+			return err
+		}
+		switch v := t.(type) {
+		case string:
+			if sanitize && prevDelim == ':' {
+				v = Mask
+				sanitize = false
+			}
+			if cnt%2 != 0 && len(ds) > 0 && ds[len(ds)-1] == '{' {
+				delim = colon
+				if _, ok := fset[v]; ok || (re != nil && re.MatchString(v)) {
+					sanitize = true
+				}
+			}
+			bw.Write(strconv.AppendQuote(tmp[:0], v))
+		case bool:
+			if v {
+				bw.WriteString("true")
+			} else {
+				bw.WriteString("false")
+			}
+		case json.Delim:
+			switch v {
+			case '{', '[':
+				ds = append(ds, rune(v))
+			case '}', ']':
+				if len(ds) > 0 {
+					ds = ds[:len(ds)-1]
+				}
+			}
+			cnt = 0
+			prevDelim = 0
+			bw.WriteRune(rune(v))
+		case json.Number:
+			bw.WriteString(string(v))
+		case nil:
+			bw.WriteString("null")
+		default:
+			return fmt.Errorf("unknown json token: %v", v)
+		}
+		cnt++
+		if dec.More() {
+			if v, ok := t.(json.Delim); !ok || v == '}' || v == ']' {
+				prevDelim = delim
+				bw.Write([]byte{delim, ' '})
+			}
+		}
+	}
+}
+
+// Message sanitizes json payload from src and returns its sanitized
+// representation. If dst is non-nil, it is used as a scratch buffer to reduce
+// allocations. fset is a set of exact field names to be filtered, re is
+// a regular expression to match against field names to be filtered. Either of
+// fset or re can be nil, but not both. Matched string fields are replaced with
+// Mask value.
+func Message(dst, src []byte, fset map[string]struct{}, re *regexp.Regexp) ([]byte, error) {
+	if re == nil && len(fset) == 0 {
+		return nil, errInvalidArguents
+	}
+	if len(dst) > 0 {
+		dst = dst[:0]
+	}
+	dec := json.NewDecoder(bytes.NewReader(src))
+	dec.UseNumber()
+	var ds []rune // stack of separators
+	var cnt int
+	var sanitize bool
+	var prevDelim byte
+	for {
+		var delim byte = comma
+		t, err := dec.Token()
+		if err == io.EOF {
+			return dst, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		switch v := t.(type) {
+		case string:
+			if sanitize && prevDelim == ':' {
+				v = Mask
+				sanitize = false
+			}
+			if cnt%2 != 0 && len(ds) > 0 && ds[len(ds)-1] == '{' {
+				delim = colon
+				if _, ok := fset[v]; ok || (re != nil && re.MatchString(v)) {
+					sanitize = true
+				}
+			}
+			dst = strconv.AppendQuote(dst, v)
+		case bool:
+			dst = strconv.AppendBool(dst, v)
+		case json.Delim:
+			switch v {
+			case '{', '[':
+				ds = append(ds, rune(v))
+			case '}', ']':
+				if len(ds) > 0 {
+					ds = ds[:len(ds)-1]
+				}
+			}
+			cnt = 0
+			prevDelim = 0
+			dst = append(dst, byte(v))
+		case json.Number:
+			dst = append(dst, string(v)...)
+		case nil:
+			dst = append(dst, "null"...)
+		default:
+			return nil, fmt.Errorf("unknown json token: %v", v)
+		}
+		cnt++
+		if dec.More() {
+			if v, ok := t.(json.Delim); !ok || v == '}' || v == ']' {
+				prevDelim = delim
+				dst = append(dst, delim, ' ')
+			}
+		}
+	}
+}
+
+// Mask replaces sensitive fields
+const Mask = "********"
+
+const (
+	comma = ','
+	colon = ':'
+)
---
+++ sanitize_test.go
@@ -0,0 +1,152 @@
+package sanitize_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/artyom/sanitize"
+)
+
+const input = `{"Msg": "Hi", "Obj": {"a":1, "c":"C", "b":null}, "Arr": ["a","b","c"], "Null": null, "Num": 1.234}`
+const want = `{"Msg": "********", "Obj": {"a": 1, "c": "********", "b": null}, "Arr": ["a", "b", "c"], "Null": null, "Num": 1.234}`
+
+var re = regexp.MustCompile(`(?i)msg$`)
+var fset = map[string]struct{}{
+	"a": struct{}{},
+	"b": struct{}{},
+	"c": struct{}{},
+}
+
+func TestStream(t *testing.T) {
+	buf := new(bytes.Buffer)
+	if err := sanitize.Stream(buf, strings.NewReader(input), fset, re); err != nil {
+		t.Fatal(err)
+	}
+	if !json.Valid(buf.Bytes()) {
+		t.Fatal("invalid output:", buf)
+	}
+	if buf.String() != want {
+		t.Log("input:", input)
+		t.Log("want:", want)
+		t.Fatal("got:", buf)
+	}
+}
+
+func TestMessage(t *testing.T) {
+	dst, err := sanitize.Message(nil, []byte(input), fset, re)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !json.Valid(dst) {
+		t.Fatal("invalid output:", string(dst))
+	}
+	if got := string(dst); got != want {
+		t.Log("input:", input)
+		t.Log("want:", want)
+		t.Fatal("got:", got)
+	}
+}
+
+func BenchmarkStream(b *testing.B) {
+	b.ReportAllocs()
+	b.SetBytes(int64(len(input)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := sanitize.Stream(ioutil.Discard, strings.NewReader(input), fset, re); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMessage(b *testing.B) {
+	dst := make([]byte, len(input))
+	b.ReportAllocs()
+	b.SetBytes(int64(len(input)))
+	b.ResetTimer()
+	var err error
+	for i := 0; i < b.N; i++ {
+		if dst, err = sanitize.Message(dst, []byte(input), fset, re); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkMessage_Custom(b *testing.B) {
+	name := os.Getenv("JSON")
+	fields := os.Getenv("FIELDS")
+	if name == "" || fields == "" {
+		b.Skip("either JSON or FIELDS environment is empty, skipping")
+	}
+	src, err := ioutil.ReadFile(name)
+	if err != nil {
+		b.Fatal(err)
+	}
+	if !json.Valid(src) {
+		b.Fatalf("file %q does not contain valid json", name)
+	}
+	fset := make(map[string]struct{})
+	for _, f := range strings.Split(fields, ",") {
+		fset[f] = struct{}{}
+	}
+	dst := make([]byte, len(src))
+	b.ReportAllocs()
+	b.SetBytes(int64(len(src)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if dst, err = sanitize.Message(dst, src, fset, nil); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkStream_Custom(b *testing.B) {
+	name := os.Getenv("JSON")
+	fields := os.Getenv("FIELDS")
+	if name == "" || fields == "" {
+		b.Skip("either JSON or FIELDS environment is empty, skipping")
+	}
+	src, err := ioutil.ReadFile(name)
+	if err != nil {
+		b.Fatal(err)
+	}
+	if !json.Valid(src) {
+		b.Fatalf("file %q does not contain valid json", name)
+	}
+	fset := make(map[string]struct{})
+	for _, f := range strings.Split(fields, ",") {
+		fset[f] = struct{}{}
+	}
+	b.ReportAllocs()
+	b.SetBytes(int64(len(src)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err = sanitize.Stream(ioutil.Discard, bytes.NewReader(src), fset, nil); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Example() {
+	msg := `{"ID": 42, "Name": "Zaphod Beeblebrox", "Secret": "Trillian"}`
+
+	// exact field name matches
+	fields := map[string]struct{}{
+		"Name": struct{}{},
+	}
+	// matches fields starting with "secret" in a case-insensitive way
+	re := regexp.MustCompile(`(?i)secret`)
+
+	out, err := sanitize.Message(nil, []byte(msg), fields, re)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+	// Output:
+	// {"ID": 42, "Name": "********", "Secret": "********"}
+}
